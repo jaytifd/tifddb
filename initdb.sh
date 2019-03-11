74c96afd84cd8a61363b2e1e154cc4ad69ec86fd
jaytifd <48419215+jaytifd@users.noreply.github.com>
2019-03-10 20:30:22 -0500

    Update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,9 +1,12 @@
-
 dbname="tifd"
+
+#an edited schema of the tifd db - remove some "?" from table names, remove some table contraints, etc.
+schema="newschema.edited"
+
 mysql -e "drop database $dbname"
 mysql -e "create database tifd"
 #mdb-schema --drop-table TIFD_DB.mdb mysql | grep -v COMMENT | mysql $dbname
-cat schema.sql | mysql $dbname
+cat $schema | mysql $dbname
 
 tables=`mdb-schema --drop-table TIFD_DB.mdb mysql | grep -v COMMENT | grep -v tblHelp | grep CREATE | cut -f2 -d\\\``
 
@@ -20,15 +23,18 @@ echo  "alter table tblCamper add index persID (persID)" | mysql $dbname
 #remove the silly "tbl" prefix and the CamelCase
 #and re-name the indexes sensibly so that symfony can use them without trouble
 
-echo "
-rename table tblAgeCategories to agecategories;
+
+IFS='
+'
+
+lines="rename table tblAgeCategories to agecategories;
 rename table tblConstantsCamp to constantscamp;
 rename table tblConstantsMembership to constantsmembership;
 rename table tblHelp to help;
 rename table tblHousingChoices to housingchoices;
 rename table tblHousingRooms to housingrooms;
 rename table tblHousingType to housingtype;
-rename table tblT-shirts to t-shirts;
+rename table \`tblT-shirts\` to tshirts;
 
 rename table tblEmailRegistered to emailregistered;
 rename table tblMembershipType to membershiptype;
@@ -42,7 +48,6 @@ rename table tblMemUnit to memunit;
 rename table tblPerson to person;
 rename table tblRegForm to regform;
 
-
 ALTER TABLE camper CHANGE CamperID id int ;
 ALTER TABLE camper CHANGE RegID regform_id int ;
 ALTER TABLE camper CHANGE PersID person_id int ;
@@ -51,7 +56,15 @@ ALTER TABLE memunit CHANGE MemID id int ;
 
 ALTER TABLE person CHANGE PersID id int ;
 ALTER TABLE person CHANGE MemID memunit_id int ;
-" | mysql $dbname
 
 ALTER TABLE regform CHANGE RegID id int ;
 ALTER TABLE regform CHANGE MemID memunit_id int ;
+"
+for l in $lines
+do
+echo "$l"
+echo $l | mysql $dbname
+
+done
+
+#" | mysql $dbname
