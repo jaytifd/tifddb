532cd52e4e20a5e0d444c3d69ef6c57ed8ce9226
jaytifd <jay@office.compound.local>
2025-02-03 14:39:07 -0600

    more camp language removed from membership form

--- camp/models.py
+++ camp/models.py
@@ -669,7 +669,7 @@ class Person(models.Model):
     instruments = models.CharField(max_length=255, blank=True, null=True)
     diet = models.BooleanField("Dietary restrictions",      blank=True, null=False, default=False)
     diet_details = models.TextField("Dietary restrictions",blank=True, null=True)
-    publish = models.BooleanField("I consent to having my contact information published in the camp roster.",        blank=True, null=False, default=False)
+    publish = models.BooleanField("I consent to having my contact information published in the roster.",        blank=True, null=False, default=False)
     staff = models.BooleanField("I am a member of TIFD's administrative staff (camp committee volunteer).",        blank=True, null=False, default=False)
     need_linen = models.BooleanField("Check here to rent linens (sheets/towels) provided by GFC",  blank=True, null=False, default=False)
     staff_position = models.CharField("Staff position(s)",max_length=255, blank=True, null=True)
