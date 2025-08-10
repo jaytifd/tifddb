557be828051ee9ce07655438c7c85c6ef07e21a9
jaytifd <jay@office.compound.local>
2025-08-10 08:29:52 -0500

    temporary fix for unicode error

--- camp/custom/mylogger.py
+++ camp/custom/mylogger.py
@@ -5,7 +5,10 @@ import inspect
 def p(*item):
     curframe = inspect.currentframe()
     frame=sys._getframe(1).f_code.co_filename+":"+str(sys._getframe(1).f_lineno)+" "+sys._getframe(1).f_code.co_name
-    print ("DEBUG: ", str(datetime.datetime.now()), " - ", frame,*item)
+    try:
+        print ("DEBUG: ", str(datetime.datetime.now()), " - ", frame,*item)
+    except:
+        print ("ERROR PRINTING")
     logfile = open('debuglog.txt', 'a')
     print ("DEBUG: ", str(datetime.datetime.now()), " - ", frame, *item, file=logfile)
     logfile.close()
